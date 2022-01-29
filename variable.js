4f34a90dab6a9960dd47278c1d2fd87e151475d7
Anand Kumar <73009664+Anandkx007@users.noreply.github.com>
2022-01-29 18:50:41 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,8 @@
+var digit1=6; // old tech
+let di=10; // new tech
+const x=10;  //con not change further;
+var y;
+console.log(x);
+console.log(di);
+console.log(digit1);
+console.log(y); // will print undefined
